78ed7e8c275704c8430c3a3263d2a1c518a166e2
brunochaves77 <bruno-mg300@hotmail.com>
2023-08-26 17:42:14 -0300

    Add Customers component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { themeSettings } from "theme";
 import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
 import Products from "scenes/products";
+import Customers from "scenes/customers";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -34,6 +35,10 @@ function App() {
               <Route
                 path="/products"
                 element={<Products />}
+              />
+              <Route
+                path="/customers"
+                element={<Customers />}
               />
             </Route>
           </Routes>
