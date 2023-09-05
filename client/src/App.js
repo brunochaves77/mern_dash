ba34fb2f24a48b0c4a01241fc78867d4b88f6d64
brunochaves77 <bruno-mg300@hotmail.com>
2023-09-04 23:20:48 -0300

    Add Transactions scene and context

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
 import Products from "scenes/products";
 import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -39,6 +40,10 @@ function App() {
               <Route
                 path="/customers"
                 element={<Customers />}
+              />
+              <Route
+                path="/transactions"
+                element={<Transactions />}
               />
             </Route>
           </Routes>
